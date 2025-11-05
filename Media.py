4ce2f73614f452cc52d1e87b7195759e89515e96
pedronastur <pedronastur@yahoo.com.br>
2025-11-05 10:58:24 -0300

    correção do codigo

---
+++ Media.py
@@ -0,0 +1,12 @@
+# Faça um programa para calcular a média de 2 notas e mostrar essa média e o nome do aluno.
+
+
+nome_aluno = input("Digite o nome do aluno: ")
+nota_1 = float(input("digite a primeira nota do aluno: "))
+nota_2 = float(input("digite a segunda nota do aluno: "))
+
+media = (nota_1 + nota_2)/2
+
+print("olá", nome_aluno, "sua média é: ", media)
+
+
